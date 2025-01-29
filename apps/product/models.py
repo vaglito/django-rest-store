ada7c0a2d656fdb67008619a6f0b892181c89e11
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-01-29 15:00:33 -0500

    feat: Add models Category and Product
    Product and Category have a relationship ForeigKey

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Category(models.Model):
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    catetegory = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    slug = models.SlugField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
