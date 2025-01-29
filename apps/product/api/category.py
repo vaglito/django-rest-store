554f166ebf1947750a8a3afac7adf59fcf11d66f
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-01-29 15:44:40 -0500

    feat: Add endpoint for category

---
+++ apps/product/api/category.py
@@ -0,0 +1,21 @@
+# Description: Category API views.
+
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from ..models import Category
+from .serializers import CategorySerializer
+
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
+class CategoryListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
+
+class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [AllowAny]
