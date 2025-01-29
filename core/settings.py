c0b36fe21783b92e718b90915c1347690a5e90a9
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-01-29 17:42:44 -0500

    feat: add cors headers

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ BASE_DEPENDENCIES = [
 OTHER_DEPENDENCIES = [
     # Add other dependencies here
     'rest_framework',
+    'corsheaders',
 ]
 
 # Local apps
@@ -58,6 +59,7 @@ INSTALLED_APPS = BASE_DEPENDENCIES + OTHER_DEPENDENCIES + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -157,3 +159,8 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', default=False, cast=bool)
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=Csv())
