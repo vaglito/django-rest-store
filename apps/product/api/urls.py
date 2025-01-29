9f52bc0667a5f98ffac8e7487d6e3f6d8864d6d9
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-01-29 15:44:21 -0500

    feat: add URL categories

--- apps/product/api/urls.py
+++ apps/product/api/urls.py
@@ -2,8 +2,14 @@
 URL configuration for product app.
 """
 from . import product
+from . import category
 from django.urls import path
 
 urlpatterns = [
     path('products/', product.ProductListAPIView.as_view(), name='product-list'),
+    path('products/create/', product.ProductListCreateAPIView.as_view(), name='product-create'),
+    path('products/<int:pk>/', product.ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+    path('categories/', category.CategoryListAPIView.as_view(), name='category-list'),
+    path('categories/create/', category.CategoryListCreateAPIView.as_view(), name='category-create'),
+    path('categories/<int:pk>/', category.CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-detail'),
 ]
