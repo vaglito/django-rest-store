f7aeb21501eedb7652184f464df9eb1c750c3f60
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-04-19 13:57:47 -0500

    fix: correct category spelling in ProductAdmin and CategoryAdmin

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active', 'price', 'catetegory', 'created_at']
+    search_fields = ['name', 'pk']
+    list_filter = ['catetegory', 'created_at']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+    search_fields = ['name', 'pk']
