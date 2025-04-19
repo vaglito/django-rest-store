208e8b6df7eead346339859529918f3cc2d82f93
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-04-19 18:32:16 -0500

    feat: add user authentication endpoint to API URLs

--- apps/authenticated/api/urls.py
+++ apps/authenticated/api/urls.py
@@ -3,9 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from . import user
 
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('user/', user.UserAuth.as_view(), name='user_auth'),
 ]
