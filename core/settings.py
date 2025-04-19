cb1b014dda8fe55315a7049ab61a77c22347b366
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-04-19 13:58:29 -0500

    fix: refactor DATABASES setting to always use environment variables

--- core/settings.py
+++ core/settings.py
@@ -96,15 +96,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': config('DB_ENGINE'),
             'NAME': config('DB_NAME'),
