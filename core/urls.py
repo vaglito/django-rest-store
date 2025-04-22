bec52c9ba80f3543c3956be11e88aa0b8a13a6f6
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-04-22 12:46:13 -0500

    feat: integrate drf-yasg for API documentation and update URL patterns

--- core/urls.py
+++ core/urls.py
@@ -15,17 +15,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 # Add the URL configuration for the product app.
 from apps.product.api import urls as product_urls
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django rest ecommerce API",
+        default_version='v1.0.0',
+        description="Ecommerce API for managing products, orders, and users.",
+        contact=openapi.Contact(email="tucorreo@dominio.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1.0/products/', include(product_urls)),
     path('api/v1.0/auth/', include('apps.authenticated.api.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
