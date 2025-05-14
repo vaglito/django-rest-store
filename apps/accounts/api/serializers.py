195bf503b2ac617db8a88e733832f24278321809
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-05-14 16:51:04 -0500

    feat: add ChangePasswordSerializer for user password management

--- apps/accounts/api/serializers.py
+++ apps/accounts/api/serializers.py
@@ -1,5 +1,23 @@
 from django.contrib.auth.models import Group, User
+from django.contrib.auth.password_validation import validate_password
+from django.core import exceptions
 from rest_framework import serializers
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate(self, data):
+        try:
+            validate_password(data['new_password'], self.context['request'].user)
+        except exceptions.ValidationError as e:
+            raise serializers.ValidationError({'new_password': list(e.messages)})
+        
+        if not self.context['request'].user.check_password(data['old_password']):
+            raise serializers.ValidationError({'old_password': 'Old password is incorrect.'})
+        
+        return data
 
 
 class UserSerializer(serializers.ModelSerializer):
