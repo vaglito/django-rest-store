4f9882e0d105c87a0966ed5d613f50acce200aef
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-06-01 13:39:25 -0500

    feat: update product API permissions to restrict access for admin users

--- apps/product/api/product.py
+++ apps/product/api/product.py
@@ -1,14 +1,19 @@
 # Description: Product API views.
 
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.renderers import JSONRenderer
 from ..models import Product
 from .serializers import ProductSerializer
 from ..filters import ProductFilter
+from apps.lib.permissions import IsAdminOrReadOnly
 
 class ProductListAPIView(generics.ListAPIView):
+    """
+    API view to retrieve the list of all active products.
+    Accessible by any user (no authentication required).
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
@@ -17,11 +22,21 @@ class ProductListAPIView(generics.ListAPIView):
     renderer_classes = [JSONRenderer]
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
+    """
+    API view to retrieve the list of all products or create a new product.
+    - GET: accessible only by admin users.
+    - POST: accessible only by admin users.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
 
 class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API view to retrieve, update, or delete a product instance.
+    - GET: accessible by any user (read-only).
+    - PUT/PATCH/DELETE: restricted to admin users only.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
